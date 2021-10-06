243750665b5d11052bd2968230ed5bc55537628b
samDobb <samdobbelaere@outlook.be>
2021-10-06 17:03:45 +0200

    Initial commit

---
+++ Server/src/ChatServer.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatServer {
+
+    private List<String> userlist = new ArrayList<>();
+    private List<ChatServerThread> clientList = new ArrayList<>();
+
+    public static void main(String[] args){
+
+    }
+}
